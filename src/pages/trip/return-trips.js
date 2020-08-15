3c0f8a3c8166d093de1a8181720227808718db80
themiraclemotors <info@themiraclemotors.com>
2020-08-15 08:53:27 -0700

    Update return-trips.js

--- src/pages/trip/return-trips.js
+++ src/pages/trip/return-trips.js
@@ -81,7 +81,7 @@ const SearchResult = props => {
           terminals,
           departureTerminalId
         )} to ${extractTerminalName(terminals, arrivalTerminalId)}`}
-        date={moment(departureTimestamp).format("MMMM D, YYYY")}
+        date={moment(departureTimestamp).format("DD MM, YYYY")}
         btnOnClick={() => navigate("/")}
       />
       <ResultWrapper>
