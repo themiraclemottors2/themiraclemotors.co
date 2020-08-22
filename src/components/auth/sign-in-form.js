f39d26c986d61351e4b4e81fcec32b322510d843
olaniyihope <55544400+OlaniyiHope@users.noreply.github.com>
2020-08-22 05:03:25 -0700

    Update sign-in-form.js

--- src/components/auth/sign-in-form.js
+++ src/components/auth/sign-in-form.js
@@ -76,7 +76,7 @@ const SignInForm = props => {
         Forgot password? Reset password <Link to="/password-reset">here.</Link>
       </p>
       <p>
-      Continue without signing in <Link to="../trip/search-results">here.</Link>
+      Continue without signing in <Link to="../trip/search-result">here.</Link>
       </p>
     </FormContainer>
   )
