56eb4d896bca9af659d9e27273ef6f38c6228ea2
themiraclemotors <info@themiraclemotors.com>
2020-09-15 03:15:46 -0700

    Update policy.js

--- src/assets/data/policy.js
+++ src/assets/data/policy.js
@@ -3,7 +3,7 @@ export default [
     header: `
 `,
     text: `
-    Customer privacy is important to Themiraclemotor.co. To better protect the privacy of cardholder data, Themiraclemotor.co provides this privacy statement explaining the security and handling practices of cardholder data in the processing of payment card transactions.<br/><br/>
+    Customer privacy is important to themiraclemotorsng.com. To better protect the privacy of cardholder data, themiraclemotorsng.com provides this privacy statement explaining the security and handling practices of cardholder data in the processing of payment card transactions.<br/><br/>
 
     This privacy statement applies to all cardholder data collected by or submitted to The Miracle motors website. The Miracle motors website will only collect personally identifiable information and cardholder data voluntarily provided by cardholders and customers, and will only use that information for the specific purposes for which it was provided. The Miracle motors will keep this information strictly confidential, and will not disclose, sell, or lease the information to third parties unless required by law, or with the written permission of the cardholder. <br/><br/>
     
@@ -12,7 +12,7 @@ export default [
     The Miracle motors-maintained websites may have links to other third party sites used for payment card transactions. These third party sites may collect cardholder data and personally identifiable information through the use of forms or cookies, or from the customer's web browser. Cardholders and customers are strongly encouraged to review the privacy policies of all third party websites outside the control of The Miracle motors for their procedures for collecting, utilising, and disclosing cardholder data.<br/><br/>
     
     The Miracle motors has made significant investment in security measures employed to protect cardholder data under its control. Access to acquired cardholder data and personally identifiable information is limited to only those personnel for whom there is an established business need to access that data. <br/><br/>
-    Themiraclemotor.co reserves the right to amend this privacy statement at any time. Thank you.<br/><br/>
+    themiraclemotorsng.com reserves the right to amend this privacy statement at any time. Thank you.<br/><br/>
     `,
   },
 ]
