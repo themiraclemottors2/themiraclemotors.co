3d424d1f5980a8838ae760f509ab8c3e532bda93
themiraclemotors <info@themiraclemotors.com>
2020-09-21 03:30:01 -0700

    Update popular-trips.js

--- src/components/home/popular-trips.js
+++ src/components/home/popular-trips.js
@@ -1,6 +1,18 @@
 import React from "react"
 import styles from "./home.module.scss"
 import { Link } from "gatsby"
+import abisaa from "../../assets/images/abisaa.jpg"
+import benin3 from "../../assets/images/benin3.jpg"
+import abuja2 from "../../assets/images/abuja2.jpg"
+import uyoooo from "../../assets/images/uyoooo.png"
+import delta from "../../assets/images/delta.png"
+import lagooo from "../../assets/images/lagooo.jpg"
+import bayelsa from "../../assets/images/bayelsa.jpg"
+import rivers from "../../assets/images/rivers.jpg"
+import anabra from "../../assets/images/anabra.jpg"
+import owerri from "../../assets/images/owerri.jpg"
+import enugu from "../../assets/images/enugu.jpg"
+import asa from "../../assets/images/asa.jpg"
 import { useDispatch, useSelector, shallowEqual } from "react-redux"
 import { capitalize } from "lib"
 import { getSearchData } from "../../store/actions/trips"
@@ -29,40 +41,55 @@ const PopularTrips = () => {
       <div className={styles.PopularTrips__TripListWrapper}>
         <ul className={styles.PopularTrips__TripList}>
           <li className={styles.PopularTrips__Trip}>
+          <img src={benin3} style={{ width: "250px" }} alt="" />
             <Link to="/#">Lagos to Benin</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
+          <img src={abuja2} style={{ width: "250px" }} alt="" />
             <Link to="/#">Lagos to Abuja</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
-            <Link to="/#">Lagos to Port Harcourt</Link>
+          <img src={uyoooo} style={{ width: "250px" }} alt="" />
+            <Link to="/#">Lagos to Calabar</Link>
           </li>
 
           <li className={styles.PopularTrips__Trip}>
+          <img src={abisaa} alt="" />
             <Link to="/#">Lagos to Warri</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
+          <img src={delta} style={{ width: "250px" }} alt="" />
             <Link to="/#">Lagos to Enugu</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
+          <img src={lagooo} style={{ width: "250px" }} alt="" />
             <Link to="/#">Lagos to Onitsha</Link>
           </li>
 
           <li className={styles.PopularTrips__Trip}>
-            <Link to="/#">Lagos to Owerri</Link>
+          <img src={enugu} style={{ width: "250px" }} alt="" />
+            <Link to="/#">Lagos to Enugu</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
+          <img src={bayelsa} style={{ width: "250px" }} alt="" />
             <Link to="/#">Lagos to Bayelsa</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
-            <Link to="/#">Abuja to Benin</Link>
+          <img src={rivers} style={{ width: "250px" }} alt="" />
+            <Link to="/#">Lagos to PH City</Link>
           </li>
 
           <li className={styles.PopularTrips__Trip}>
-            <Link to="/#">Abuja to Port Harcourt</Link>
+          <img src={anabra} style={{ width: "250px" }} alt="" />
+            <Link to="/#">Lagos to Awka</Link>
           </li>
           <li className={styles.PopularTrips__Trip}>
-            <Link to="/#">Abuja to Lagos</Link>
+          <img src={owerri} style={{ width: "250px" }} alt="" />
+            <Link to="/#">Lagos to Owerri</Link>
+          </li>
+          <li className={styles.PopularTrips__Trip}>
+          <img src={asa} style={{ width: "250px" }} alt="" />
+            <Link to="/#">Lagos to Asaba</Link>
           </li>
         </ul>
       </div>
